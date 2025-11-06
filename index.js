1858fdae944a0ca56fbce4ecde11582045cf07a6
Prabhu1403 <prabhubsccs24@gmail.com>
2025-11-06 20:39:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let input=document.querySelector("input")
+
+function getClick(inputVal){
+    input.value += inputVal
+    
+}
+
+function delVal(params) {
+    input.value=input.value.slice(0,input.value.length-1)
+    
+}
+
+function Clearval(params) {
+ input.value=""
+}
+
+function Return(params) {
+    input.value=eval(input.value)
+}
